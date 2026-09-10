a02fb75b6df8ee8d9cf1aae09931d9a3f885e5d8
Matheus Nobre <matheus1204h@gmail.com>
2026-09-10 15:10:12 -0300

    MS-009: add card component

---
+++ manto-surpresa-frontend/app/components/cardBox/CardBox.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image"
+import Link from "next/link"
+
+interface CardBoxProps {
+    image: string
+    name: string
+    description: string
+    price: number
+}
+
+export default function CardBox({
+    image, 
+    name,
+    description,
+    price
+}: CardBoxProps) {
+    return (
+        <article className="w-70 max-w-sm px-4 py-4 rounded-lg shadow-lg pb-6 transition hover:scale-102">
+            <Link href="/box">
+            
+            <Image
+                src={image}
+                alt={name}
+                width={350}
+                height={350}
+                className="w-full h-auto rounded-md"
+            />
+            <h2 className="text-primary text-2xl font-bold mt-4">
+                {name}
+            </h2>
+
+            <p className="text-justify mt-2 h-30 text-sm">
+                {description}
+            </p>
+
+            <div className="flex flex-col items-end gap-1">
+                <p className="items-end text-text-secondary text-sm">
+                    R$ 
+                    <span className="ml-1 text-primary text-bold text-lg">
+                        {price.toLocaleString("pt-BR", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        })}
+                    </span>
+                </p>
+                <button className="bg-primary rounded-md text-white text-sm p-3 transition hover:scale-105">
+                    Comprar agora
+                </button>
+            </div>
+            </Link>
+        </article>
+    )
+}
--- manto-surpresa-frontend/app/page.tsx
+++ manto-surpresa-frontend/app/page.tsx
@@ -1,6 +1,28 @@
+import CardBox from "./components/cardBox/CardBox";
+
 export default function PageHome() {  return (
     <main>
-        <h1>Home</h1>
+        <div className="flex flex-wrap items-center justify-center gap-8 p-8">
+          <CardBox
+              image="/boxes/box_classica.png"
+              name="Box Clássica"
+              description="Na Box Clássica, você pode receber a camisa de um clube ou seleção, seja retrô ou atual. É a surpresa perfeita para os amantes do futebol, sendo a box principal da Manto Surpresa!"
+              price={179.90}
+          />
+          <CardBox
+              image="/boxes/box_retro.png"
+              name="Box Retrô"
+              description="Na Box Retrô, a surpresa continua, mas há uma certeza: a camisa recebida será uma retrô, agradando fãs nostálgicos e que desejam relembrar times históricos do passado."
+              price={199.90}
+          />
+          <CardBox
+              image="/boxes/box_selecao.png"
+              name="Box Seleção"
+              description="A Box Seleção é ideal para quem deseja adquirir uma camisa de uma seleção nova. Oportunidade perfeita para ter não somente uma nova camisa, mas conhecer, quem sabe, uma nova cultura ou país."
+              price={179.90}
+          />
+        </div>
+        
     </main>
   );
 }
