4c43b013f6ad07418d6a393cd7d2ebf683db3a02
Matheus Nobre <matheus1204h@gmail.com>
2026-09-15 16:51:00 -0300

    MS-022: connect front with back to get boxes from API

--- manto-surpresa-frontend/app/page.tsx
+++ manto-surpresa-frontend/app/page.tsx
@@ -1,48 +1,41 @@
 import CardBox from "./components/cardBox/CardBox";
 import Image from "next/image";
+import { getBoxes } from "./services/boxService";
+import { Box } from "./types/box";
 
-export default function PageHome() {  return (
-    <main>
-        <section className="min-h-[calc(100vh-4rem)] flex flex-wrap gap-8 justify-center px-8 py-8">
-          <CardBox
-              id={1}
-              image="/boxes/box_classica.png"
-              name="Box Clássica"
-              description="Na Box Clássica, você pode receber a camisa de um clube ou seleção, seja retrô ou atual. É a surpresa perfeita para os amantes do futebol, sendo a box principal da Manto Surpresa!"
-              price={179.90}
-          />
-          <CardBox
-              id={2}
-              image="/boxes/box_retro.png"
-              name="Box Retrô"
-              description="Na Box Retrô, a surpresa continua, mas há uma certeza: a camisa recebida será uma retrô, agradando fãs nostálgicos e que desejam relembrar times históricos do passado."
-              price={199.90}
-          />
-          <CardBox
-              id={3}
-              image="/boxes/box_selecao.png"
-              name="Box Seleção"
-              description="A Box Seleção é ideal para quem deseja adquirir uma camisa de uma seleção nova. Oportunidade perfeita para ter não somente uma nova camisa, mas conhecer, quem sabe, uma nova cultura ou país."
-              price={179.90}
-          />
-        </section>
+export default async function PageHome() {  
+    const boxes = await getBoxes()
 
-        <section className="flex flex-col bg-secondary w-full py-8 px-4">
-            <div className="flex justify-center">
-                <h2 className="text-primary font-bold text-2xl">
-                    Manto Surpresa
-                </h2>
-            </div>
-
-            <div className="flex items-center justify-center p-4 gap-8">
-                <Image 
-                    src="/1.png"
-                    alt="Manto Surpresa Entregue"
-                    width={400}
-                    height={400}
+    return (
+        <main>
+            <section className="min-h-[calc(100vh-4rem)] flex flex-wrap gap-8 justify-center px-8 py-8">
+            {boxes.map((box: Box) => (
+                <CardBox key={box.id}
+                    id = {box.id}
+                    image = {box.image}
+                    name = {box.name}
+                    description = {box.description}
+                    price = {box.price}
                 />
-            </div>
-        </section>
-    </main>
-  );
+            ))}
+            </section>
+
+            <section className="flex flex-col bg-secondary w-full py-8 px-4">
+                <div className="flex justify-center">
+                    <h2 className="text-primary font-bold text-2xl">
+                        Manto Surpresa
+                    </h2>
+                </div>
+
+                <div className="flex items-center justify-center p-4 gap-8">
+                    <Image 
+                        src="/1.png"
+                        alt="Manto Surpresa Entregue"
+                        width={400}
+                        height={400}
+                    />
+                </div>
+            </section>
+        </main>
+    );
 }
---
+++ manto-surpresa-frontend/app/services/boxService.tsx
@@ -0,0 +1,23 @@
+import { Box } from "../types/box";
+
+const API_URL = process.env.API_URL;
+
+export async function getBoxes(): Promise<Box[]> {
+  const response = await fetch(`${API_URL}/box`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch boxes");
+  }
+
+  return response.json();
+}
+
+export async function getBoxById(id: number): Promise<Box> {
+    const response = await fetch(`${API_URL}/box/${id}`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch boxes");
+    }
+
+    return response.json();
+}
