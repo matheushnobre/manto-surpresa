d2806bd2dd098cd36432f211f18030492793f3d2
Matheus Nobre <matheus1204h@gmail.com>
2026-09-15 17:39:14 -0300

    MS-022: adjust frontend CI to test build

--- manto-surpresa-frontend/app/services/boxService.ts
+++ manto-surpresa-frontend/app/services/boxService.ts
@@ -3,7 +3,7 @@ import { Box } from "../types/box";
 const API_URL = process.env.API_URL;
 
 export async function getBoxes(): Promise<Box[]> {
-  const response = await fetch(`${API_URL}/box`);
+  const response = await fetch(`${API_URL}/box`, {next: { revalidate: 3600 }});
 
   if (!response.ok) {
     throw new Error("Failed to fetch boxes");
@@ -13,7 +13,7 @@ export async function getBoxes(): Promise<Box[]> {
 }
 
 export async function getBoxById(id: number): Promise<Box> {
-    const response = await fetch(`${API_URL}/box/${id}`);
+    const response = await fetch(`${API_URL}/box/${id}`, { next: {revalidate: 3600 }});
 
     if (!response.ok) {
         throw new Error("Failed to fetch boxes");
